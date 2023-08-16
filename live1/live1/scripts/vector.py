b3f9c13d8768d557f3eabb28351b8852b9b40a55
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-16 19:32:24 -0300

    modificação na mensagem de platoon action

--- live1/live1/scripts/vector.py
+++ live1/live1/scripts/vector.py
@@ -98,12 +98,12 @@ def logCAN(s):
         log['radarDistLong'] = ((((msgECU[7] << 8) + msgECU[8]) >> 3)*0.2) - 500
         log['radarDistLat'] = ((((msgECU[8] & 0x03) << 8) + msgECU[9])*0.2) - 102.3
 
-    elif msgECU[2] == 0x97:
+    elif msgECU[2] == 0x98:
         log['ECU'] = 'Comunicacao - Platoon Action'
         #log['Reservado'] = msgECU[4]
-        log['ID visitante'] = msgECU[10]
-        log['Posicao'] = msgECU[11]
-        log['Action'] = msgECU[12]
+        log['ID visitante'] = msgECU[11]
+        log['Posicao'] = msgECU[12]
+        log['Action'] = msgECU[13]
     else:
         log['ECU'] = 'ZERO'
         log['Angulo'] = msgECU[1]
@@ -215,7 +215,7 @@ def sendMsg(s, msgCANId, value):
             mesg[13] = value[5]      #RpmEsq
             mesg[14] = value[6]      #RpmDir
 
-        elif msgCANId == 0x98:
+        elif msgCANId == 0x97:
             mesg[0]  = 1
             mesg[12] = value[0]      #Direção
             mesg[13] = value[1]      #RpmEsq
